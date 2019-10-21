815e5c9a91b33f2f653f5ee20cc72c3a1be864df
atsushiflee <atsushiflee@icloud.com>
2019-10-20 22:42:32 -0700

    get title

--- voice_recognition/collect_voice_data.py
+++ voice_recognition/collect_voice_data.py
@@ -2,16 +2,39 @@ import urllib.request as req
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver import Firefox, FirefoxOptions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-main_url = "https://librivox.org/search?primary_key=35&search_category=language&search_page=1&search_form=get_results"
+url_list = []
+n = 1
 
 # Firefox headless
 options = FirefoxOptions()
-options.add_argument('_headless')
+# options.add_argument('--headless')
 
 # open Firefox
 browser = Firefox(options=options)
-# open url
-browser.get(main_url)
+while True:
+    main_url = ("https://librivox.org/search?primary_key=35&search_category=language&search_page=%d&search_form=get_results" % n)
 
-ui = browser.find_element_by_css_selector("body > div > div.browse.browse-title > ul")
+    # open url
+    browser.get(main_url)
+    WebDriverWait(browser, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "book-author")))
+    # get li tag
+    li = browser.find_elements_by_css_selector("body > div > div.browse.browse-title > ul > li")
+
+    # add each url to list
+    for i in li:
+        url = i.find_elements_by_css_selector("h3 > a")[0].get_attribute("href")
+        # check duplicate
+        if url not in url_list:
+            url_list.append(url)
+
+
+    n += 1
+    if n == 6:
+        break
+
+print(len(url_list))
