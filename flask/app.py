f1130052e245dcdfbc78fcc80eb7f4006a1f1620
atsushiflee <atsushiflee@icloud.com>
2020-02-03 10:53:00 -0800

    define model

--- flask/app.py
+++ flask/app.py
@@ -1,13 +1,17 @@
 from flask import Flask, request, render_template
-from utils.text_preprocess import *
-from utils.model import *
-from utils.dataset import *
-
-
+from flask.load_token_dic import load_english_dic, load_japanese_dic
+from flask.utils.text_preprocess import normalize_english
+from flask.utils.dataset import tokenize
+from flask.utils.model import decoder_seq
+from load_token_dic import *
 import tensorflow as tf
 
 # define model
-# model = tf.keras.models.load_model("saver")
+model = tf.keras.models.load_model("path")
+
+# load word token dic
+english_tokens = load_english_dic()
+japanese_tokens = load_japanese_dic()
 
 
 app = Flask(__name__)
@@ -21,6 +25,7 @@ def index():
 @app.route("/translation", methods=["POST", "GET"])
 def translate():
     result = {}
+
     if request.method == "POST":
 
         # input lang
@@ -30,12 +35,10 @@ def translate():
         # translate lang
         input_lang = normalize_english(input_lang)
         # create input value for encode
-        encoder_input = convert_to_nparray(input_lang)
+        encoder_input = tokenize(input_lang, english_tokens)
 
         # predict
-        result["predict"] = decoder_seq(model, encoder_input)
-
-
+        result["predict"] = decoder_seq(model, encoder_input, japanese_tokens)
 
         return render_template("translate.html", lang=result)
 
