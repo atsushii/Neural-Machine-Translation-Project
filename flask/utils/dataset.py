68edd16ec9f402a82e41eeb64b10917f0e9e2481
atsushiflee <atsushiflee@icloud.com>
2020-02-04 22:45:08 -0800

    add

--- flask/utils/dataset.py
+++ flask/utils/dataset.py
@@ -1,22 +1,24 @@
 import config as cfg
 import numpy as np
 
-def convert_to_nparray(input_lang):
+
+def tokenize(input_lang, english_dic):
     """
     convert input value to vector
 
     :param input_lang: String, English language
+    :param english_dic: dictionary, tokenize dictionary
     :return: encoder input
     """
 
-    # max length of input sentense
+    # max length of input sentence
     english_len = len(input_lang.split())
 
     encoder_input_data = np.zeros((english_len, cfg.MAX_INPUT_SIZE), dtype="float32")
 
     # tokenize
     for i , word in enumerate(input_lang.split()):
-        encoder_input_data[1, i] = token_dict[word]
+        encoder_input_data[1, i] = english_dic[word]
 
     return encoder_input_data
 
