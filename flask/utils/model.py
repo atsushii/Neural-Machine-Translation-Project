76f9e8e7c271a00da50af066495333f907971a4b
atsushiflee <atsushiflee@icloud.com>
2020-02-04 22:44:42 -0800

    Get error tyr model._make_prediction

--- flask/utils/model.py
+++ flask/utils/model.py
@@ -1,42 +1,40 @@
 import tensorflow as tf
 import config as cfg
 import numpy as np
+import pdb
 
 
-def encoder_model(model):
+def create_model(model):
 
     # encoder model
-    encoder_input = model.input[0]  # enocoder input
+    encoder_input = model.input[0]  # encoder input
     encoder_output, state_h, state_c = model.layers[6].output  # encoder lstm
     encoder_state = [state_h, state_c]
-    encoder_model = tf.keras.Model(encoder_input, encoder_state)
+    encoder_m = tf.keras.models.Model(encoder_input, encoder_state)
 
-    return encoder_model
-
-
-def decoder_model(model):
     # decoder model
     decoder_input = model.input[1]  # decoder input
-    decoder_state_input_h = tf.keras.Input(shape=(cfg.UNITS,))
-    decoder_state_input_c = tf.keras.Input(shape=(cfg.UNITS,))
+    decoder_state_input_h = tf.keras.layers.Input(shape=(cfg.UNITS,))
+    decoder_state_input_c = tf.keras.layers.Input(shape=(cfg.UNITS,))
     decoder_state_input = [decoder_state_input_h, decoder_state_input_c]
-    decoder_emb = model.layers[3]
-    decoder_emb = decoder_emb(decoder_input)
+    decoder_emb_layer = model.layers[3]
+    decoder_emb = decoder_emb_layer(decoder_input)
     decoder_lstm = model.layers[7]  # decoder lstm
     decoder_output, dec_state_h, dec_state_c = decoder_lstm(decoder_emb, initial_state=decoder_state_input)
     decoder_state = [dec_state_h, dec_state_c]
 
     decoder_dense = model.layers[16]
-    decoder_output = decoder_dense(decoder_output)
+    decoder_outputs = decoder_dense(decoder_output)
 
-    decoder_model = tf.keras.Model([decoder_input] + decoder_state_input, [decoder_output] + decoder_state)
+    decoder_m = tf.keras.models.Model([decoder_input] + decoder_state_input, [decoder_outputs] + decoder_state)
 
-    return decoder_model
+    return encoder_m, decoder_m
 
 
-def decoder_seq(model, input_seq, japanese_tokens):
+def decoder_seq(encoder, decoder, input_seq, japanese_tokens):
+    pdb.set_trace()
     # encoder the input seq as vector
-    state_en = encoder_model(model).predict(input_seq)
+    state_en = encoder.predict(input_seq)
     # generate empty target sequence
     target_seq = np.zeros((1, cfg.MAX_OUTPUT_SIZE))
     # populate the first character of target seq
@@ -47,7 +45,7 @@ def decoder_seq(model, input_seq, japanese_tokens):
     decoder_sentence = ' '
 
     while not stop_condition:
-        output_token, h, c = decoder_model(model).predict([target_seq] + state_en)
+        output_token, h, c = decoder.predict([target_seq] + state_en)
 
         sampled_token_index = np.argmax(output_token[0, -1, :])
         sampled_char = japanese_tokens[sampled_token_index]
