ed84db2740f14ed6934538d10112d95a4a2c61a1
atsushiflee <atsushiflee@icloud.com>
2020-02-05 23:03:15 -0800

    Fix graph error

--- flask/app.py
+++ flask/app.py
@@ -2,20 +2,23 @@ from flask import Flask, request, render_template
 from load_token_dic import load_english_dic, load_japanese_dic
 from utils.text_preprocess import normalize_english
 from utils.dataset import tokenize
-from utils.model import decoder_seq, create_model
+from utils.model import Predict, CreateModel
 import tensorflow as tf
-
-app = Flask(__name__)
 
 # define model
 model = tf.keras.models.load_model("saver/model/model.h1.22_Nov_19")
 
 # create model
-encoder, decoder = create_model(model)
-
 # load word token dic
 english_tokens = load_english_dic()
 japanese_tokens = load_japanese_dic()
+model_ = CreateModel(model)
+
+encoder, decoder = model_()
+
+predict = Predict(encoder, decoder, japanese_tokens)
+
+app = Flask(__name__)
 
 
 @app.route("/")
@@ -40,7 +43,7 @@ def translate():
         encoder_lang = tokenize(normalize_lang, english_tokens)
 
         # predict
-        result["predict"] = decoder_seq(encoder, decoder, encoder_lang, japanese_tokens)
+        result["predict"] = predict.result(encoder_lang)
 
         return render_template("translate.html", lang=result)
 
