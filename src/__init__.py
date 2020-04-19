be2128a19c040380a39697d9a1b9ad1446d4f933
Atsushi Miyamoto <atsushiflee@icloud.com>
2020-04-19 16:17:47 -0700

    Delete __init__.py

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-from src import views
-from flask import Flask, Blueprint
-import tensorflow as tf
-from .views.views import view_blueprint
-
-app = Flask(__name__, static_url_path="/static")
-app.register_blueprint(view_blueprint)
