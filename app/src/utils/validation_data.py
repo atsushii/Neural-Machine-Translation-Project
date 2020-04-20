273934d613347b6a06c9b9f6e09e2f34b40dee43
atsushiflee <atsushiflee@icloud.com>
2020-04-19 17:45:46 -0700

    Validate input text

---
+++ app/src/utils/validation_data.py
@@ -0,0 +1,21 @@
+class Validation():
+    """
+    Validate input data
+
+    return: bool
+    """
+
+    def __init__(self, data, token):
+        self.data = data
+        self.token = token
+
+    def valdation_data(self):
+        if not "input" in self.data.keys():
+            return False
+
+        for i in self.data["input"].split():
+
+            if not i.strip() in self.token.word_index:
+                return False
+
+        return True
